8918e3ea45379ab4e291418d4395c023c216548a
Maintainer Dashboard <dashboard@users.noreply.github.com>
2026-08-10 08:48:37 +0000

    Upgrade to Laravel 13

--- src/StubsPublishCommand.php
+++ src/StubsPublishCommand.php
@@ -4,34 +4,22 @@ declare(strict_types=1);
 
 namespace TheDoctor0\Stubs;
 
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
-use Illuminate\Filesystem\Filesystem;
 use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\File;
 use Symfony\Component\Finder\SplFileInfo;
 
+#[Description('Publish all opinionated stubs that are available for customization')]
+#[Signature('stubs:publish {--force : Overwrite any existing files}')]
 class StubsPublishCommand extends Command
 {
     use ConfirmableTrait;
 
     /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'stubs:publish {--force : Overwrite any existing files}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Publish all opinionated stubs that are available for customization';
-
-    /**
      * Execute the console command.
-     *
-     * @return int
      */
     public function handle(): int
     {
@@ -45,7 +33,7 @@ class StubsPublishCommand extends Command
 
         $override = $this->option('force');
 
-        collect(File::files(__DIR__ . '/../stubs'))
+        collect(File::files(__DIR__.'/../stubs'))
             ->each(fn (SplFileInfo $file) => $this->copy($file, $stubsPath, $override));
 
         $this->info('Stubs published!');
@@ -55,16 +43,10 @@ class StubsPublishCommand extends Command
 
     /**
      * Copy file to specified path.
-     *
-     * @param \Symfony\Component\Finder\SplFileInfo $file
-     * @param string                                $path
-     * @param bool                                  $override
-     *
-     * @return void
      */
     protected function copy(SplFileInfo $file, string $path, bool $override): void
     {
-        $targetPath = $path . "/{$file->getFilename()}";
+        $targetPath = $path."/{$file->getFilename()}";
         $sourcePath = $file->getPathname();
 
         if ($override || ! file_exists($targetPath)) {
